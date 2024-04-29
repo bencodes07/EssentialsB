5546e97c085429cadae42108c8a48494df1062d1
Ben Böckmann <boeckmannben@gmail.com>
2024-04-29 16:06:55 +0200

    Minor adjustments

--- src/main/java/de/bencodes/commands/Command_Pos.java
+++ src/main/java/de/bencodes/commands/Command_Pos.java
@@ -70,7 +70,12 @@ public class Command_Pos implements CommandExecutor, TabCompleter {
                                 objective.getScore("§7 X: §a" + xValue).setScore(4);
                                 objective.getScore("§7 Y: §a" + yValue).setScore(3);
                                 objective.getScore("§7 Z: §a" + zValue).setScore(2);
-                                objective.getScore("§7 World: §a" + (config.getString("Position." + posName + ".World").split("_")[1]).toString().substring(0,1).toUpperCase() + config.getString("Position." + posName + ".World").split("_")[1].toString().substring(1)).setScore(1); // Remove world_ prefix
+                                if(config.getString("Position." + posName + ".World").contains("world_")) {
+                                    objective.getScore("§7 World: §a" + (config.getString("Position." + posName + ".World").split("_")[1]).toString().substring(0,1).toUpperCase() + config.getString("Position." + posName + ".World").split("_")[1].toString().substring(1)).setScore(1); // Remove world_ prefix
+                                } else {
+                                    objective.getScore("§7 World: §aOverworld").setScore(1);
+                                }
+
                                 p.setScoreboard(scoreboard);
                             } else {
                                 p.sendMessage(Essentials.pre + "§cDu hast keine Berechtigung für diese Position!");
@@ -105,13 +110,35 @@ public class Command_Pos implements CommandExecutor, TabCompleter {
                                 config.set("Position." + posName + ".Public", isPublic);
                                 Essentials.getInstance().saveConfig();
                                 p.sendMessage(Essentials.pre + "§6Die Position ist nun " + (isPublic ? "öffentlich" : "privat") + ".");
-                            } else if (modificationType.equalsIgnoreCase("displayName")) {
-                                String newDisplayName = args[3];
-                                config.set("Position." + posName + ".DisplayName", newDisplayName);
-                                Essentials.getInstance().saveConfig();
-                                p.sendMessage(Essentials.pre + "§6Der Name der Position wurde geändert.");
                             } else {
-                                p.sendMessage(Essentials.pre + "§cInvalid modification type. Use 'public true/false' or 'displayName <NewDisplayName>'.");
+                                p.sendMessage(Essentials.pre + "§cInvalid modification type. Use 'public true/false' or 'location'.");
+                            }
+                        } else {
+                            p.sendMessage(Essentials.pre + "§cDu hast keine Berechtigung um diese Position zu modifizieren!");
+                        }
+                    } else {
+                        p.sendMessage(Essentials.pre + "§cDiese Position existiert nicht!");
+                    }
+                } else if (args.length == 3 && args[0].equalsIgnoreCase("modify")) {
+                  String posName = args[1];
+                    if (config.contains("Position." + posName)) {
+                        String playerForPosition = config.getString("Position." + posName + ".Player");
+                        if (playerForPosition.equals(p.getName())) {
+                            String modificationType = args[2];
+                            if (modificationType.equalsIgnoreCase("public")) {
+                                boolean isPublic = Boolean.parseBoolean(args[3]);
+                                config.set("Position." + posName + ".Public", isPublic);
+                                Essentials.getInstance().saveConfig();
+                                p.sendMessage(Essentials.pre + "§6Die Position ist nun " + (isPublic ? "öffentlich" : "privat") + ".");
+                            } else if (modificationType.equalsIgnoreCase("location")) {
+                                config.set("Position." + posName + ".X", p.getLocation().getX());
+                                config.set("Position." + posName + ".Y", p.getLocation().getY());
+                                config.set("Position." + posName + ".Z", p.getLocation().getZ());
+                                config.set("Position." + posName + ".World", p.getLocation().getWorld().getName());
+                                Essentials.getInstance().saveConfig();
+                                p.sendMessage(Essentials.pre + "§6Die Position wurde aktualisiert");
+                            } else {
+                                p.sendMessage(Essentials.pre + "§cInvalid modification type. Use 'public true/false' or 'location'.");
                             }
                         } else {
                             p.sendMessage(Essentials.pre + "§cDu hast keine Berechtigung um diese Position zu modifizieren!");
@@ -156,13 +183,14 @@ public class Command_Pos implements CommandExecutor, TabCompleter {
                             completions.add(posName);
                         }
                     }
+                    completions.removeIf(s -> !s.toLowerCase().startsWith(args[1].toLowerCase()));
                     Collections.sort(completions);
                 } else if (args[0].equalsIgnoreCase("save")) {
                     completions.add("<PosName>");
                 }
             } else if (args.length == 3 && args[0].equalsIgnoreCase("modify")) {
                 completions.add("public");
-                completions.add("displayName");
+                completions.add("location");
             } else if (args.length == 4 && args[0].equalsIgnoreCase("modify")) {
                 if (args[2].equalsIgnoreCase("public")) {
                     completions.add("true");
